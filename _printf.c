a598e6385f357eae00480bcfcc2da67838344a5e
immercer <nanakkay@gmail>
2023-04-27 21:42:47 +0000

    handling cases of %, %s, %c

---
+++ _printf.c
@@ -0,0 +1,61 @@
+#include <stdarg.h>
+#include "main.h"
+#include <stdio.h>
+
+int _printf(const char *format, ...)
+{
+	int char_count = 0;
+	int i = 0;
+
+	va_list args;
+	va_start(args, format);
+	for (; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch(format[i])
+			{
+				case 'c':
+				{
+					char c = va_arg(args, int);
+					_putchar(c);
+					char_count++;
+					break;
+				}
+				case 's':
+				{
+					char *s = va_arg(args, char *);
+					while (*s)
+					{
+						_putchar(*s);
+						char_count++; s++; 
+					}
+					break;
+				}
+				case '%':
+				{
+					_putchar('%');
+					char_count++;
+					break;
+				}
+			}	
+		}
+		else
+		{
+			_putchar(format[i]);
+			char_count++;
+		}
+	}
+	va_end(args);
+
+	return char_count;
+	
+}
+
+int main(void)
+{
+	_printf("hello");
+	_putchar('\0');
+	_printf("%%");
+}
--- main.h
+++ main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
+int printf(const char *format, ...);
 
 #endif /* end of MAIN_H */
