8337bcb3f7b9bc289bb0203f6edb6404769301d3
immercer <nanakkay@gmail>
2023-04-28 03:17:24 +0000

    update

--- _putchar.c
+++ _putchar.c
@@ -10,3 +10,23 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ *
+ * Return: the number of characters printed
+ */
+int _puts(char *str)
+{
+    int len = 0;
+
+    while (*str)
+    {
+        len += write(1, str, 1);
+        str++;
+    }
+
+    return len;
+}
--- main.h
+++ main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+
 int _putchar(char c);
-int printf(const char *format, ...);
+int _printf(const char *format, ...);
+char* (*get_func(char c))(va_list);
+int _puts(char *str);
 
 #endif /* end of MAIN_H */
