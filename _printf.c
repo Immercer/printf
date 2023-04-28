92351cdfc3aa9f4cadc3299253266d2e00093be5
immercer <nanakkay@gmail>
2023-04-28 04:06:04 +0000

    betty style

--- _printf.c
+++ _printf.c
@@ -10,62 +10,64 @@
 
 int _printf(const char *format, ...)
 {
-    int char_count = 0;
-    int i = 0;
-    va_list args;
+	int char_count = 0;
+	int i = 0;
+	va_list args;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format[i])
-    {
-        if (format[i] == '%' && format[i + 1] != '\0')
-        {
-            if (format[i + 1] == 'c')
-            {
-                char c = va_arg(args, int);
-                _putchar(c);
-                char_count++;
-                i++;
-            }
-            else if (format[i + 1] == 's')
-            {
-                char *s = va_arg(args, char *);
-                if (s != NULL)
-                {
-                    while (*s)
-                    {
-                        _putchar(*s);
-                        char_count++;
-                        s++;
-                    }
-                }
-                else
-                {
-                    _puts("(null)");
-                    char_count += 6;
-                }
-                i++;
-            }
-            else if (format[i + 1] == '%')
-            {
-                _putchar('%');
-                char_count++;
-                i++;
-            }
-            else
-            {
-                _putchar(format[i]);
-                char_count++;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            char_count++;
-        }
-        i++;
-    }
-    va_end(args);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			if (format[i + 1] == 'c')
+			{
+				char c = va_arg(args, int);
 
-    return (char_count);
+				_putchar(c);
+				char_count++;
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				char *s = va_arg(args, char *);
+
+				if (s != NULL)
+				{
+					while (*s)
+					{
+						_putchar(*s);
+						char_count++;
+						s++;
+					}
+				}
+				else
+				{
+					_puts("(null)");
+					char_count += 6;
+				}
+				i++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				char_count++;
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				char_count++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			char_count++;
+		}
+		i++;
+	}
+	va_end(args);
+
+	return (char_count);
 }
